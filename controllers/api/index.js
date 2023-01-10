e9d239423636dcaf367034c46f149300507ac990
Benjamin Sokolik <sokolik_b@hotmail.com>
2023-01-10 15:24:51 -0700

    small changes to handful of files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,18 +1,11 @@
 const router = require('express').Router();
-
-
-// const apiRoutes = require('./api');
 const noteRoutes = require('./noteRoutes');
 const userRoutes = require('./userRoutes');
 const commentRoutes = require('./commentRoutes');
-//  "homeRoutes" should be "../homepageRoutes?"
 const homepageRoutes = require('../homepageRoutes');
 
 router.use('/users', userRoutes);
 router.use('/notes', noteRoutes);
-// router.use('/comments', commentRoutes);
-
-// router.use('./api', apiRoutes);
-
+router.use('/comments', commentRoutes);
 
 module.exports = router;
