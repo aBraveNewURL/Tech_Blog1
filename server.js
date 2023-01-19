9c9d97e2eab3d6c82b6ca9842b1524cbe7eac181
Benjamin Sokolik <sokolik_b@hotmail.com>
2023-01-18 18:51:02 -0700

    refactoring server, updated package file

--- server.js
+++ server.js
@@ -1,57 +1,44 @@
-// Required modules
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-// Required files
-const routes = require('./controllers');
-const helpers = require('./utils/helper');
-const sequelize = require('./config/connection');
-
-// Setup sessions
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// Setup express and server port
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Direct handlebars to helper files
-const hbs = exphbs.create({ helpers });
-
-// Session parameters
 const sess = {
-	secret: process.env.SESSION_SECRET,
-	cookie: {
-		// maxAge: 150000,
-		// httpOnly: true,
-		// secure: false,
-		// sameSite: 'strict',
-	},
-	resave: false,
-	saveUninitialized: true,
-	store: new SequelizeStore({
-		db: sequelize
-	})
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
-// Use the session
 app.use(session(sess));
 
-// Connect express and handlebars
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// Setup express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Setup static path to public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Use routes
 app.use(routes);
 
-// Sequelize and server listening
 sequelize.sync({ force: false }).then(() => {
-	app.listen(PORT, () => console.log(`Server is up and running at ${PORT}!`));
+  app.listen(PORT, () => console.log('Now listening'));
 });
